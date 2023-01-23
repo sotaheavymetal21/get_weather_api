8e8df03fea475b391516d540f03f0743da950171
sotaheavymetal21 <sota_anegawa@pj100.biz>
2023-01-23 16:47:09 +0900

    色々追加

--- weather_project/weather/views.py
+++ weather_project/weather/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Weather
+from .serializers import WeatherSerializer
 
 # Create your views here.
 
@@ -9,3 +12,9 @@ from .models import Weather
 class TopView(ListView):
     model = Weather
     template_name = "top.html"
+
+
+class WeatherAPIView(RetrieveAPIView):
+    queryset = Weather.objects.all()
+    serializer_class = WeatherSerializer
+    permission_classes = [IsAuthenticated]
